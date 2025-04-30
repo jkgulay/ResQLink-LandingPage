c142a401e4356027484e19834ae7ccccb625b5fb
Kayel2 <gulayjunkyle@gmail.com>
2025-05-01 02:23:11 +0800

    added mdi fonts

--- resqlink/src/main.js
+++ resqlink/src/main.js
@@ -4,6 +4,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 // vuetify
+import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
@@ -15,10 +16,12 @@ import router from './router'
 const app = createApp(App)
 
 const vuetify = createVuetify({
-    components,
-    directives,
-  })
-  
+  icons: {
+    defaultSet: 'mdi',
+  },
+  components,
+  directives,
+})
 
 app.use(createPinia())
 app.use(router)
